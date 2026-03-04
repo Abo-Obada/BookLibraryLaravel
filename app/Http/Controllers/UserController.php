50f0e83c1ab7a04f691fd67dcf0fe374e40d61c5
Abo Obada <abooyaman999@gmail.com>
2026-03-04 20:13:33 +0300

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,29 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
 class UserController extends Controller
 {
    public function login(Request $request)
 {
+   
     $credentials = $request->validate([
         'email'    => 'required|email',
         'password' => 'required',
     ]);
     
-   if(!$credentials){
-      return response()->json(['message','error']);
+   if(!Auth::attempt($credentials)) {
+      throw ValidationException::withMessages([
+            'message' => ['The provided credentials are incorrect.'],
+        ]);
+
    }
+
    Auth::setRememberDuration(43200);
    Auth::attempt($credentials, true);
-}
-
-public function logout(){
-  
-}
-
-
-   public function user(){
-      return response()->json(['user token data']);
+   return response()->json(['user'=>Auth::user()->only('email','username')]);
    }
-  
 }
+
