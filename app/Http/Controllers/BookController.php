8a82f2785ebe29dfd164ad6f6b7c7a45a561c0bc
Abo-Obada <abooyaman999@gmail.com>
2026-03-10 13:13:57 +0300

    added new features

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function getAllBooks(Request $request){
+    public function getAllBooks(){
     $bookCovers = BookCover::select([
         'uuid',
         'book_name'
@@ -24,31 +24,49 @@ class BookController extends Controller
     return response()->json($bookCovers);
     }
 
-    public function categorizeBook(){
-    $filter = preg_match('/^[\p{Arabic}a-zA-Z\p{N}]+\h?[\p{N}\p{Arabic}a-zA-Z]*$/u', request('category'));
-    if($filter == 1){
-        $query = request('category');
-    }
-    else {
-        $query = [];
-    }
-    $bookCategory = Category::where('category_name','like',$query)->get();
-    $category =   $bookCategory->pluck("id");
+//     public function categorizeBook(){
+//     $query = request('cq');
+//     $bookCategory = Category::where('category_name','like',$query)->get();
+//     $category =   $bookCategory->pluck("id");
+
+//     $bookCovers = BookCover::select(
+// ['uuid','book_name','book_image',
+//         'book_rate','book_page_number','created_at',
+//         'book_description','views'
+//         ]
+//         );
+
+//     if($query == "all"){
+//       $bookCovers =  $bookCovers->paginate(20);
+//     }else{
+//     $bookCovers = $bookCovers->where("category_id",'like', [$category])->paginate(20);
+
+//     }
+//     return response()->json($bookCovers);
+//     }
+
+
+public function categorizeBook()
+{
+    $query = request()->validated()['cq'] ?? 'all'; // use Form Request ideally
+
     $bookCovers = BookCover::select([
-        'uuid',
-        'book_name'
-        ,'book_image',
-        'book_rate',
-        'book_page_number',
-        'created_at',
-        'book_description',
-        'views'
-        ],"book")->where("category_id",'like', [$category])->paginate(5);
-        return response()->json($bookCovers);
+        'uuid', 'book_name', 'book_image', 'book_rate',
+        'book_page_number', 'created_at', 'book_description', 'views'
+    ]);
+
+    if ($query !== 'all') {
+        $bookCovers->whereHas('category', function ($q) use ($query) {
+            $q->where('category_name', 'like', '%' . $query . '%');
+        });
     }
+
+    return response()->json($bookCovers->paginate(5));
+}
 
     public function getCategory(){
         $category = Category::select(['category_name','uuid'])->get();
+        return response()->json($category);
     }
 }
 
