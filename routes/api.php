80e491dc03ab7ed32658a9954d36668ff94b9cb2
Abo Obada <abooyaman999@gmail.com>
2026-04-04 12:57:17 +0300

    fix username and logging out

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function (){
 Route::get('me', [UserController::class,'me']);
-Route::post('logout',[BookController::class,'logout']);
+Route::post('logout',[UserController::class,'logout']);
 Route::post('createcomment/{uuid}',[BookController::class,'createComment']);
 Route::put('createreaction/{uuid}',[BookController::class,'createReaction']);
 });
